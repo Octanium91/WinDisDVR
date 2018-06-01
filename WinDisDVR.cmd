190a97cf8ed9154fc8b4ea0dbba4151d09063183
Octanium91 <octanium91@gmail.com>
2018-06-01 22:34:59 +0300

    FIX [ERROR #18404]
    On some systems script crashed
    
    Now it will show you the error message "Sorry but script can not be applied in this system!"
    
    ERROR #18404

--- WinDisDVR.cmd
+++ WinDisDVR.cmd
@@ -2,9 +2,9 @@
 
 :LANG
 for /f "tokens=3" %%a in ('reg query "HKEY_CURRENT_USER\Control Panel\International" /v Locale') do set "LANGKB=%%a"
-if "%LANGKB%"=="00000409" goto LANG_ENG
-if "%LANGKB%"=="00000419" goto LANG_RUS
-if "%LANGKB%"=="00000422" goto LANG_RUS
+if %LANGKB% == 00000409 goto LANG_ENG
+if %LANGKB% == 00000419 goto LANG_RUS
+if %LANGKB% == 00000422 goto LANG_RUS
 goto LANG_ENG
 :LANG_END
 
@@ -12,9 +12,9 @@ goto LANG_ENG
 set "RMA=%RANDOM%"
 set "RMB=%RANDOM%"
 mkdir "%WINDIR%\CHECK_ADMIN_ACCESS_%RMA%%RMB%"
-if "%ERRORLEVEL%"=="1" (set "ADMIN_AC=1") else (set "ADMIN_AC=0"&rmdir "%WINDIR%\CHECK_ADMIN_ACCESS_%RMA%%RMB%" /s /q)
+if %ERRORLEVEL% == 1 (set ADMIN_AC=1) else (set ADMIN_AC=0&rmdir "%WINDIR%\CHECK_ADMIN_ACCESS_%RMA%%RMB%" /s /q)
 cls
-if "%ADMIN_AC%"=="1" (
+if %ADMIN_AC% == 1 (
 title=Windows DVR Disabeler [%MES_NOACCESS%]
 echo.
 echo  %MES_ADMIN%
@@ -32,20 +32,37 @@ echo.
 set "regbackUpFile=%~dp0RegBackUpFile.reg"
 set "regbackUpTEMPFile=%temp%RegBackUpFile.tmp"
 set "DVR_ENEBELED=0"
+set "regAppCaptureEnable=null"
+set "regGameDVR=null"
+set "regAllowgameDVR=null"
 for /f "tokens=2*" %%a in ('REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR /v AppCaptureEnabled') do set "regAppCaptureEnable=%%b"
-for /f "tokens=2*" %%a in ('REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR /v AudioCaptureEnabled') do set "regAudioCaptureEnabled=%%b"
 for /f "tokens=2*" %%a in ('REG QUERY HKCU\System\GameConfigStore /v GameDVR_Enabled') do set "regGameDVR=%%b"
 for /f "tokens=2*" %%a in ('REG QUERY HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowGameDVR /v value') do set "regAllowgameDVR=%%b"
+
+if %regAppCaptureEnable% == null GOTO ERROR18404
+if %regGameDVR% == null GOTO ERROR18404
+if %regAllowgameDVR% == null set GOTO ERROR18404
+
 if %regAppCaptureEnable% == 0x1 set "DVR_ENEBELED=1"
 if %regGameDVR% == 0x1 set "DVR_ENEBELED=1"
 if %regAllowgameDVR% == 0x1 set "DVR_ENEBELED=1"
-if %regAudioCaptureEnabled% == 0x1 set "DVR_ENEBELED=1"
 
 if %DVR_ENEBELED% == 1 (
 	GOTO IsENABELED
 ) else (
 	GOTO IsDISABELED
 )
+
+:ERROR18404
+cls
+echo.
+echo   Sorry but script can not be applied in this system!
+echo   ERROR #18404
+echo.
+echo  %LPAKFE%
+echo.
+pause
+exit
 
 :IsENABELED
 cls
@@ -66,7 +83,7 @@ echo   %LNWDVRDIS%
 echo.
 echo  %LPAKFE%
 echo.
-pause
+pause 
 exit
 
 :DISDVR
@@ -78,29 +95,25 @@ echo  [*] %LCBF%
 if exist "%regbackUpFile%" set "regbackUpFile=%~dp0RegBackUpFile_%RANDOM%%RANDOM%%RANDOM%%RANDOM%.reg"
 echo Windows Registry Editor Version 5.00>>"%regbackUpFile%"&echo.>>"%regbackUpFile%"&echo ; Registry file generated in WinDisDVR.cmd>>"%regbackUpFile%"&echo.>>"%regbackUpFile%"
 echo [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\GameDVR]>>"%regbackUpFile%"
-reg export HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR "%regbackUpTEMPFile%" /y&& type "%regbackUpTEMPFile%" | findstr "AppCaptureEnabled">>"%regbackUpFile%"
-reg export HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR "%regbackUpTEMPFile%" /y&& type "%regbackUpTEMPFile%" | findstr "AudioCaptureEnabled">>"%regbackUpFile%"&echo.>>"%regbackUpFile%"
+reg export HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR "%regbackUpTEMPFile%" /y&& type "%regbackUpTEMPFile%" | findstr "AppCaptureEnabled">>"%regbackUpFile%"&echo.>>"%regbackUpFile%"
 echo [HKEY_CURRENT_USER\System\GameConfigStore]>>"%regbackUpFile%"
 reg export HKCU\System\GameConfigStore "%regbackUpTEMPFile%" /y&& type "%regbackUpTEMPFile%" | findstr "GameDVR_Enabled">>"%regbackUpFile%"&echo.>>"%regbackUpFile%"
 echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowGameDVR]>>"%regbackUpFile%"
 reg export HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowGameDVR "%regbackUpTEMPFile%" /y&& type "%regbackUpTEMPFile%" | findstr "value">>"%regbackUpFile%"&echo.>>"%regbackUpFile%"
 echo  [*] %LLDWD%
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR /v AppCaptureEnabled /t REG_DWORD /d 0 /f
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR /v AudioCaptureEnabled /t REG_DWORD /d 0 /f
 REG ADD HKCU\System\GameConfigStore /v GameDVR_Enabled /t REG_DWORD /d 0 /f
 REG ADD HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowGameDVR /v value /t REG_DWORD /d 0 /f
 echo.
 echo  [*] %LLC%
 set "CDVR_ENEBELED=0"
 for /f "tokens=2*" %%a in ('REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR /v AppCaptureEnabled') do set "cregAppCaptureEnable=%%b"
-for /f "tokens=2*" %%a in ('REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR /v AudioCaptureEnabled') do set "cregAudioCaptureEnabled=%%b"
 for /f "tokens=2*" %%a in ('REG QUERY HKCU\System\GameConfigStore /v GameDVR_Enabled') do set "cregGameDVR=%%b"
 for /f "tokens=2*" %%a in ('REG QUERY HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowGameDVR /v value') do set "cregAllowgameDVR=%%b"
 
 if %cregAppCaptureEnable% == 0x1 set "CDVR_ENEBELED=1"
 if %cregGameDVR% == 0x1 set "CDVR_ENEBELED=1"
 if %cregAllowgameDVR% == 0x1 set "CDVR_ENEBELED=1"
-if %cregAudioCaptureEnabled% == 0x1 set "CDVR_ENEBELED=1"
 
 if %CDVR_ENEBELED% == 0 (
 	GOTO DONEOK
